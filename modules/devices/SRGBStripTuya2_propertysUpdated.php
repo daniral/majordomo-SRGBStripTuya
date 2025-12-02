39e8927812e7a0a04d37a38dac8ea6be81de7eed
daniral <daniralgal@gmail.com>
2025-12-02 21:53:01 +0200

    к

--- modules/devices/SRGBStripTuya2_propertysUpdated.php
+++ modules/devices/SRGBStripTuya2_propertysUpdated.php
@@ -125,6 +125,7 @@ $saveProperty = function ($obj) use ($property, $value, $source) {
 		$obj->setProperty('flag', 1);
 		$obj->setProperty($property . 'Saved', $value);
 	}
+    // Блокируем входящие данные от Туя на 8 сек
 	$obj->setProperty('blockTuya', 1);
 	setTimeOut($obj->object_title.'blockTuyaTimer', "setGlobal('" . $obj->object_title . ".blockTuya', 0);", 8);
 	// Если значение реально изменилось — сохраняем
